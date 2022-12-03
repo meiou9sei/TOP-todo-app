072c638808be2a457b012a6fce2ef8c670b7dada
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-02 19:15:07 -0500

    create footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("test");
+import Footer from "./components/Footer";
+
+function content() {
+  const content = document.querySelector("#content");
+
+  const footer = Footer();
+  content.appendChild(footer);
+
+  return content;
+}
+
+document.body.appendChild(content());
