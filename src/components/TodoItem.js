3fa4c2bfb065bc0794f8aa1ee0604d8dbfe324dc
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-04 19:58:55 -0500

    expand on basic todo item component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,18 @@
-export default function TodoItem(title /*, description, dueDate, priority*/) {
-  const todoItem = document.createElement("div");
-  todoItem.classList.add("todo-item");
+export default function TodoItem(todoItem) {
+  const todoItemTemplate = document.getElementById("todoItem-template");
+  const todoItemElement = document.importNode(todoItemTemplate.content, true);
+  const checkbox = todoItemElement.querySelector("input");
+  checkbox.id = todoItem.id;
+  checkbox.checked = todoItem.complete;
+  const label = todoItemElement.querySelector("label");
+  label.htmlFor = todoItem.id;
+  label.append(todoItem.title);
+  const description = todoItemElement.querySelector(".todoItem-description");
+  description.append(todoItem.description);
+  const dueDate = todoItemElement.querySelector(".todoItem-dueDate");
+  dueDate.append(todoItem.dueDate);
+  const priority = todoItemElement.querySelector(".todoItem-priority");
+  priority.append(todoItem.priority);
 
-  //parts of a todo item
-  const todoTitle = document.createElement("h3");
-  todoTitle.classList.add("todo-item-title");
-  todoTitle.innerText = title;
-
-  //attaching to master element
-  todoItem.appendChild(todoTitle);
-
-  return todoItem;
+  return todoItemElement;
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import TodoItem from "./components/TodoItem";
 import Footer from "./components/Footer";
 
+let testTodoItem = {
+  id: 1,
+  title: "make macaroni",
+  description: "I'm gonna make some yummy macaroni tonight",
+  dueDate: "24/03/2020",
+  priority: 3,
+  complete: false,
+};
+
 function content() {
   const content = document.querySelector("#content");
 
-  const testTodoItem = TodoItem("make macaroni");
-  content.appendChild(testTodoItem);
+  content.appendChild(TodoItem(testTodoItem));
 
   const footer = Footer();
   content.appendChild(footer);
