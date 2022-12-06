7ca034ad0456c0a232b8cbeb6b9e5e32e14dd6b5
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-06 10:43:43 -0500

    create todoItemsArray, list them all out in Todos container

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,6 @@
 export default function Project(projectsArray) {
   const projectElement = document.createElement("ul");
   projectElement.classList.add("project-item");
-  console.log(projectsArray);
   projectsArray.forEach((project) => {
     const projectItem = document.createElement("li");
     projectItem.textContent = project.name;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import TodoItems from "./components/TodoItems";
 import Projects from "./components/Projects";
 
+const todoItemsArray = [
+  {
+    id: 1,
+    project: "cooking",
+    title: "make macaroni",
+    description: "I'm gonna make some yummy macaroni tonight",
+    dueDate: "24/03/2023",
+    priority: 2,
+    complete: false,
+  },
+  {
+    id: 2,
+    project: "inbox",
+    title: "check email",
+    description: "gotta delete some spam mail",
+    dueDate: "22/04/2023",
+    priority: 4,
+    complete: true,
+  },
+];
+
 let testTodoItem = {
   id: 1,
   project: "cooking",
@@ -28,6 +49,8 @@ let testProject = {
 };
 
 function displayContent() {
+  // prob separate these into their own util functions / modules later
+
   //displays projects - just pass in all the arrays
   const projectsListContainer = document.querySelector(
     ".projects-list-container"
@@ -37,8 +60,11 @@ function displayContent() {
 
   //displays todos
   const todosContainer = document.querySelector(".todos-container");
-  const currentlySelectedProject = TodoItems(testTodoItem);
-  todosContainer.appendChild(currentlySelectedProject);
+  todoItemsArray.forEach((todoItem) => {
+    // worry about filtering them later
+    // todoItem.project ===
+    todosContainer.appendChild(TodoItems(todoItem));
+  });
 
   return todosContainer;
 }
