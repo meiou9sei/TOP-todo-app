8e844c8169e7f4f4d2271403c65323ff262b658b
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-06 11:37:58 -0500

    seperate data to own module for now

--- src/index.js
+++ src/index.js
@@ -3,40 +3,8 @@ import "./styles.css";
 import TodoItems from "./components/TodoItems";
 import Projects from "./components/Projects";
 
-const todoItemsArray = [
-  {
-    id: 1,
-    project: "cooking",
-    title: "make macaroni",
-    description: "I'm gonna make some yummy macaroni tonight",
-    dueDate: "24/03/2023",
-    priority: 2,
-    complete: false,
-  },
-  {
-    id: 2,
-    project: "inbox",
-    title: "check email",
-    description: "gotta delete some spam mail",
-    dueDate: "22/04/2023",
-    priority: 4,
-    complete: true,
-  },
-];
-
-const projectsArray = [
-  {
-    id: 0,
-    name: "inbox",
-    active: true,
-  },
-  {
-    id: 1,
-    name: "cooking",
-    active: false,
-  },
-];
-//make inbox project id: 0 and delete stuff above 0
+//test data to delete later
+import { todoItemsArray, projectsArray } from "./testData";
 
 function displayContent() {
   // prob separate these into their own util functions / modules later
---
+++ src/testData.js
@@ -0,0 +1,33 @@
+export const todoItemsArray = [
+  {
+    id: 1,
+    project: "cooking",
+    title: "make macaroni",
+    description: "I'm gonna make some yummy macaroni tonight",
+    dueDate: "24/03/2023",
+    priority: 2,
+    complete: false,
+  },
+  {
+    id: 2,
+    project: "inbox",
+    title: "check email",
+    description: "gotta delete some spam mail",
+    dueDate: "22/04/2023",
+    priority: 4,
+    complete: true,
+  },
+];
+
+export const projectsArray = [
+  {
+    id: 0,
+    name: "inbox",
+    active: true,
+  },
+  {
+    id: 1,
+    name: "cooking",
+    active: false,
+  },
+];
